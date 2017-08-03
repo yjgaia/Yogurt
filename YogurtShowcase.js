c1608779474d902588d4edda0402e559b9c8c300
Hanul <hanul@hanul.me>
2017-08-03 15:47:22 +0900

    설정 추가

--- YogurtShowcase.js
+++ YogurtShowcase.js
@@ -6,6 +6,9 @@ BOOT({
 		isDevMode : true,
 		webServerPort : 8881
 	},
+	BROWSER_CONFIG : {
+		// Yogurt : {...}
+	},
 	NODE_CONFIG : {
 		// 테스트 목적이기 때문에 CPU 클러스터링 기능을 사용하지 않습니다.
 		isNotUsingCPUClustering : true
